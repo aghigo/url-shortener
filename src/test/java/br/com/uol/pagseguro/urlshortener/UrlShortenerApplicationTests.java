3b8db2c0a3af94f19003b1b3f3ac7faaa3aebd4c
André Mendes Ghigo <andre.ghigo@colaborador.inpi.gov.br>
2020-04-08 15:31:04 -0300

    Initial commit

---
+++ src/test/java/br/com/uol/pagseguro/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uol.pagseguro.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
