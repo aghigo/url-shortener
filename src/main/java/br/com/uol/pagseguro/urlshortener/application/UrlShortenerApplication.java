223eef9945d647fbbe6422edcea4ce89990d217a
André Mendes Ghigo <andreghigo@gmail.com>
2020-04-19 13:12:32 -0300

    Prepare to Deploy on AWS

--- src/main/java/br/com/uol/pagseguro/urlshortener/application/UrlShortenerApplication.java
+++ src/main/java/br/com/uol/pagseguro/urlshortener/application/UrlShortenerApplication.java
@@ -3,6 +3,8 @@ package br.com.uol.pagseguro.urlshortener.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -23,8 +25,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "br.com.uol.pagseguro.urlshortener.model.entity")
 @EnableJpaRepositories("br.com.uol.pagseguro.urlshortener.repository")
 @PropertySource(value = "classpath:application.properties")
-public class UrlShortenerApplication {
+public class UrlShortenerApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UrlShortenerApplication.class);
 	}
 }
