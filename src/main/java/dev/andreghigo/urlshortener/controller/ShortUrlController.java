8e9271136bd3dbdd6fbb84f4e8ec1fadd86bd19f
André Mendes Ghigo <andreghigo@gmail.com>
2020-04-23 23:13:35 -0300

    configure cors

--- src/main/java/dev/andreghigo/urlshortener/controller/ShortUrlController.java
+++ src/main/java/dev/andreghigo/urlshortener/controller/ShortUrlController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin("*")
 @Api(value="Short URL")
 public class ShortUrlController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ShortUrlController.class);
